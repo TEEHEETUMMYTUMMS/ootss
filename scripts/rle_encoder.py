ad6e4b602e317eb97d2837ef91ad2e3ca8d57411
joric <joric@users.noreply.github.com>
2026-06-23 17:26:22 +0500

    updated files

--- scripts/rle_encoder.py
+++ scripts/rle_encoder.py
@@ -2,12 +2,12 @@ import re, shutil
 
 rle = lambda s: re.sub(r'(.)\1*', lambda m: m[1] + str(len(m[0]) if len(m[0]) > 1 else ''), s)
 
-fname = '..data/solutions.txt'
+fname = '../data/solutions.txt'
 
 shutil.copy2(fname, 'solutions.txt.bak')
 
 with open(fname) as f:
-    rows = [l.split(',', 1) for l in f.read().splitlines() if l]
+    rows = [l.split(':', 1) for l in f.read().splitlines() if l]
 
 with open(fname, 'w') as f:
     f.writelines(f"{n},{rle(s)}\n" for n, s in rows)
